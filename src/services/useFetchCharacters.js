fc0d169365e93f55b2304122ee7ed205dcdce738
alvarogarciaabasolo <alvarogarciaabasolo@gmail.com>
2024-06-26 23:25:42 +0200

    Implement localStorage for caching character data

--- src/services/useFetchCharacters.js
+++ src/services/useFetchCharacters.js
@@ -8,13 +8,30 @@ export const useFetchCharacters = (searchQuery, pageNumber) => {
   useEffect(() => {
     const fetchCharacters = async () => {
       setIsLoading(true);
+
+      const cacheKey = searchQuery || `All list page ${pageNumber}`;
+      const cachedData = localStorage.getItem(cacheKey);
+
+      if (cachedData) {
+        const parsedData = JSON.parse(cachedData);
+        setCharacters(parsedData.results);
+        setTotalPages(parsedData.totalPages);
+        setIsLoading(false);
+        return;
+      }
+
       try {
         const response = await fetch(
           `https://swapi.dev/api/people/?search=${searchQuery}&page=${pageNumber}`
         );
         const data = await response.json();
+        const cacheValue = {
+          results: data.results,
+          totalPages: Math.ceil(data.count / 10),
+        };
+        localStorage.setItem(cacheKey, JSON.stringify(cacheValue));
         setCharacters(data.results);
-        setTotalPages(Math.ceil(data.count / 10));
+        setTotalPages(cacheValue.totalPages);
       } catch (err) {
         console.error('Error fetching names:', err);
       } finally {
